b399ee6bd5a8f984996460298c6ccc7f4826e341
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2023-10-06 00:56:31 +0200

    pwa

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { VitePWA } from 'vite-plugin-pwa'
+export default defineConfig({
+  plugins: [
+    VitePWA({ 
+      injectRegister: 'auto',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,ogg}']
+      },
+      includeAssets: ['juego.svg','pwa-192x192.png', 'pwa-512x512.png'],
+      manifest: {
+        name: 'Demo Phaser',
+        short_name: 'Demo Phaser',
+        description: 'Un demo de Phaser',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
